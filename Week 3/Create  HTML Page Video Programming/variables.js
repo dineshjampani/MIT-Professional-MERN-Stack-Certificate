ae54c5ca6cf205c1e1e9b621ecea938a58e6161a
Dinesh Jampani <dineshjampani@gmail.com>
2021-01-12 10:38:47 -0500

    week 2 computational thinking work

---
+++ Week 3/Create  HTML Page Video Programming/variables.js
@@ -0,0 +1,20 @@
+/*Instructions
+ - Greet your friend by printing a message to the console.
+*/
+//your code here
+
+var myFriend = "Tyler";
+///////////////
+function greetings() {
+  //also your code here
+  ///////////////
+  return("Greetings " + myFriend +".")
+}
+
+//leave this line unchanged to console log the results
+console.log('results: ', greetings());
+
+//don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = greetings;
+}
